fa138ca6a476a80e243e7f38d9eb0870cdf3744b
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-11-23 11:27:01 -0600

    file templates for feb2018

---
+++ Silver/February-2018-Silver/1.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+using namespace std;
+
+class Solution {
+    private:
+    public:
+        void readIn() {
+            ifstream fin ("");
+        }
+
+        void main() {
+            readIn();
+            ofstream fout ("");
+        }
+};
+
+int main() {
+    Solution s;
+    s.main();
+}
