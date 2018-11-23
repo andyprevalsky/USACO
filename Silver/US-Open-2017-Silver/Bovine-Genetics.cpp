46c3894783fc00909889539e670505b03d63359b
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-11-22 21:50:13 -0600

    Bovine-Genetics USOPEN2017S

--- Silver/US-Open-2017-Silver/Bovine-Genetics.cpp
+++ Silver/US-Open-2017-Silver/Bovine-Genetics.cpp
@@ -7,46 +7,46 @@ using namespace std;
 
 class Solution {
     private:
-        struct VectorHash {
-            size_t operator() (const vector<char> &vc) const
-            {
-                return hash<string>() (string(vc.cbegin(), vc.cend()));
-            }
-        };
-
+        int memo[64];
+        int total = 0;
     public:
         void BovineGenomics() {
             ifstream fin ("cownomics.in");
             ofstream fout ("cownomics.out");
             int N, M;
             fin >> N >> M;
-            vector<string> cows;
+            vector<vector<int> > cows;
             char c;
+            int a;
             for (int i = 0; i < 2*N; i++) {
-                string cow;
+                vector<int> row;
                 for (int j = 0; j < M; j++) {
                     fin >> c;
-                    cow += c;
+                    if (c == 'A') a = 0;
+                    else if (c == 'C') a = 1;
+                    else if (c == 'G') a = 2;
+                    else a = 3;
+                    row.push_back(a);
                 }
-                cows.push_back(cow);
+                cows.push_back(row);
             }
-            int total = 0;
 
             for (int i = 0; i < M-2; i++) {
                 for (int j = i + 1; j < M-1; j++) {
                     for (int k = j+1; k < M; k++) {
-                        unordered_set<vector<char>, VectorHash> substrings;
+                        for (int i = 0; i < 64; i++) {
+                            memo[i] = 0;
+                        }
                         for (int x = 0; x < N; x++) {
-                            substrings.insert({cows[x][i], cows[x][j], cows[x][k]});
+                            memo[cows[x][i]*16 + cows[x][j]*4 + cows[x][k]] = 1;
                         }
                         for (int x = N; x < 2*N; x++) {
-                            if (substrings.find({cows[x][i], cows[x][j], cows[x][k]}) != substrings.end()) break;
+                            if (memo[cows[x][i]*16 + cows[x][j]*4 + cows[x][k]]) break;
                             if (x == 2*N - 1) total++;
                         }
                     }
                 }
             }
-            cout << total << endl;
             fout << total;
         }
 };
