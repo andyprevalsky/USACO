8da4ab823cec7358ee1a751c7cbe00c1e8aa3ec1
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-11-23 21:11:45 -0600

    Milk-Pails Feb2016S

--- Silver/February-2016-Silver/Milk-Pails.cpp
+++ Silver/February-2016-Silver/Milk-Pails.cpp
@@ -1,55 +1,66 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <unordered_set>
+#include <queue>
 using namespace std;
 
 class Solution {
     private:
-       int pailOne;
-       int pailTwo;
-       int moves;
-       int target;
-       int closest = 0;
+        int X, Y, K, M; //x = pailoneSize, y = pailtwoSize, k = maxiterations, m = target milk level
+        int closest = 10000;
+        struct VectorHash {
+            size_t operator()(const std::vector<int>& v) const {
+                std::hash<int> hasher;
+                size_t seed = 0;
+                for (int i : v) {
+                    seed ^= hasher(i) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+                }
+                return seed;
+            }
+        };
+        unordered_set<vector<int>, VectorHash> memo;
+        queue<vector<int> > q;
+
     public:
-        int main() {
+        void readIn() {
             ifstream fin ("pails.in");
-            fin >> pailOne >> pailTwo >> moves >> target;
-            if (pailOne > pailTwo) { // pail one has the smaller size
-                int tmp = pailTwo;
-                pailTwo = pailOne;
-                pailOne = tmp;
-            }
-            if (target == pailOne || target == pailTwo || (target == pailOne + pailTwo && moves >= 2)) return 0;
+            fin >> X >> Y >> K >> M;    
+        }
 
-            if (target < pailOne) {
-                closest = pailOne-target;
-                int exchanges = 1;
-                int tPailTwo = pailTwo;
-                while (exchanges+2 <= moves && tPailTwo > 0) {
-                    tPailTwo -= pailOne;
-                    if (abs(tPailTwo-target) < closest) closest = abs(tPailTwo-target);
+        void main() {
+            readIn();
+            ofstream fout ("pails.out");
+            q.push({0, 0});
+            int iterations = 0;
+            while(iterations <= K) {
+                int size = q.size();
+                for (int i = 0; i < size; i++) {
+                    vector<int> node = q.front();
+                    q.pop();
+                    if (memo.find(node) != memo.end()) continue;
+                    if (abs(M-node[0]-node[1]) < closest) {
+                        closest = abs(M-node[0]-node[1]);
+                    }
+                    memo.insert(node);
+                    q.push({X, node[1]});
+                    q.push({node[0], Y});
+                    q.push({0, node[1]});
+                    q.push({node[0], 0});
+                    if (node[0] + node[1] > X) q.push({X, node[1]-(X-node[0])});
+                    else q.push({node[0]+node[1], 0});
+                    if (node[1] + node[0] > Y) q.push({node[0]-(Y-node[1]), Y});
+                    else q.push({0, node[0]+node[1]});
                 }
+                iterations++;
             }
 
-            if (target < pailTwo) {
-                closest = min(target-pailOne, pailTwo-target);
-                int exchanges = 1;
-                int tPailOne = 0;
-                int tPailTwo = pailTwo;
-                while (exchanges+2 <= moves && tPailTwo > pailOne) {
-                    cout << tPailTwo << endl;
-                    tPailTwo -= pailOne;
-                    if (abs(tPailTwo-target) < closest) closest = abs(tPailTwo-target);
-                }
-            }
 
-            cout << closest << endl;
-            return closest;
+            fout << closest << endl;
         }
 };
 
 int main() {
-    ofstream fout ("pails.out");
     Solution s;
-    fout << s.main();
+    s.main();
 }
