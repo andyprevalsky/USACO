fee3fe322b564e2261334460d8e943a380bd0e54
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-11-28 13:11:00 -0600

    Teleporation Feb2018S

--- Silver/February-2018-Silver/Teleportation.cpp
+++ Silver/February-2018-Silver/Teleportation.cpp
@@ -2,28 +2,46 @@
 #include <algorithm>
 #include <vector>
 #include <iostream>
+#include <map>
 #include <queue>
 using namespace std;
 
 class Solution {
     private:
         int numPoints;
-        long long totalDist = 0; 
+        long long upShift = 0; 
+        long long minDist;
         vector<vector<int>> points;
-
+        map<int, int> slopes;
     public:
+
         void readIn() {
             ifstream fin ("teleport.in");
             fin >> numPoints;
             int a, b;
             for (int i = 0; i < numPoints; i++) {
                 fin >> a >> b;
-                if (abs(a-b) <= abs(a)) totalDist += abs(a-b);
-                else { 
-                    points.push_back({a, b});
+                upShift += abs(a-b);
+
+                if (abs(a-b) <= abs(a)) continue;
+                slopes[b] += 2;
+                if (a >= 0 && a >= b) {
+                    slopes[2*b]--;
+                    slopes[0]--;
+                }
+                else if (a >= 0 && a < b) {
+                    slopes[2*a]--;
+                    slopes[-2*a+2*b]--;
+                }
+                else if (a < 0 && a >= b) {
+                    slopes[2*b-2*a]--;
+                    slopes[2*a]--;
+                }
+                else if (a < 0 && a < b) {
+                    slopes[0]--;
+                    slopes[2*b]--;
                 }
             }
-            sort(points.begin(), points.end(), [](vector<int> a, vector<int> b){return a[1] <  b[1];});
         }
 
 
@@ -31,6 +49,18 @@ class Solution {
         void main() {
             readIn();
             ofstream fout ("teleport.out"); 
+            minDist = upShift;
+            long long currY = upShift;
+            long long currSlope = 0;
+            long long prevX = -2000000000;
+            for (auto i: slopes) {
+                int currX = i.first;
+                currY += (currX - prevX) * currSlope;
+                if (currY < minDist) minDist = currY;
+                currSlope += i.second;
+                prevX = currX;
+            }
+            fout << minDist << endl;
 
         }
 };
