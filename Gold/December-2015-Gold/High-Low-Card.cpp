c1ff269ee0c7259f039634d255395f26aca01e9b
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-11-28 22:48:31 -0600

    High-Low Dec2015G

--- Gold/December-2015-Gold/High-Low-Card.cpp
+++ Gold/December-2015-Gold/High-Low-Card.cpp
@@ -32,62 +32,9 @@ class Solution {
                 if (i > 2*N) break;
                 if (memo[i] == 0) B.push_back(i);
             }
-            sort(E1.begin(), E1.end());
-            sort(E2.begin(), E2.end(), greater<int>());
+            sort(E1.begin(), E1.end(), greater<int>());
+            sort(E2.begin(), E2.end());
             sort(B.begin(), B.end());
-        }
-
-        int binSearch(int lowBound1, int lowBound2) {
-            // if l2 is > l1 and nothing found, then pop the smallest card larger than l1
-            // else: pop the largest card
-            int lowBound = max(lowBound1, lowBound2);
-            int l = 0;
-            int r = B.size()-1;
-
-            int min = B[r]+1;
-            int mid = l + (r-l)/2;
-            int index = r;
-            
-            while (l < r-1) {
-                int mid = l + (r-l)/2;
-                if (B[mid] < lowBound) l = mid;
-                else {
-                    r = mid;
-                    if (B[mid] < min) {
-                        min = B[mid];
-                        index = mid;
-                    }
-                } 
-            }
-            if (B[r] < min && B[r] > lowBound) {
-                min = B[r];
-                index = r;
-            }
-            if (B[mid] < min && B[mid] > lowBound) {
-                index = mid;
-                min = B[mid];
-            }
-            if (B[l] < min && B[l] > lowBound) {
-                index = l;
-                min = B[l];
-            }
-
-            if (lowBound1 > lowBound2 && min == B[r]+1) min = B[r];
-            else if (lowBound2 > lowBound1 && min == B[r]+1) return 0;
-            // cout << "MIN: " <<  min << " LOWERBOUND: " << lowBound << endl;
-            // for (auto i: B) {
-            //     cout << i << endl;
-            // }
-            // cout << " ----- " << endl
-            B.erase(B.begin()+index);
-
-                
-                
-            // for (auto i: B) {
-            //     cout << i << endl;
-            // }
-            // cout << endl << endl;
-            return min;
         }
 
         void main() {
@@ -95,39 +42,26 @@ class Solution {
             ofstream fout ("cardgame.out");
             int points = 0;
             int j = 0;
-            int i = 0;
-            while (i != N/2 && j != N/2) {
-                int res = binSearch(E1[i], E2[j]);
-                if (res == 0) {
+            for (int i = 0; i < N/2; i++) {
+                if (j == E1.size()) break;
+                while (B[i] > E2[j]) {
                     j++;
-                    continue;
+                    if (j == E2.size()) break;
                 }
-                else if (res > E1[i]) points++;
-                i++;
+                if (j == E2.size()) break;
+                points++;
+                j++;
             }
-            while (i < N/2) {
-                int res = binSearch(E1[i], 0);
-                if (res > E1[i]) points++;
-                i++;
-            }
-            // cout << B.size() << endl;
+            reverse(B.begin(), B.end());
 
-            // cout << " " << B.size() << " " << N/2 << endl;
-            // exit(1);
-            sort(B.begin(), B.end(), greater<int>());
             j = 0;
             for (int i = 0; i < N/2; i++) {
-                if (j == B.size()) {
-                    fout << points << endl;
-                    return;
-                }               
-                while(B[j] > E2[i]) {
+                if (j == E1.size()) break;
+                while (B[i] < E1[j]) {
                     j++;
-                    if (j == B.size()) {
-                        fout << points << endl;
-                        return;
-                    }
+                    if (j == E1.size()) break;
                 }
+                if (j == E1.size()) break;
                 points++;
                 j++;
             }
