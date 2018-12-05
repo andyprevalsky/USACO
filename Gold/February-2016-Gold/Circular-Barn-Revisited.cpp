f4429177c93ec47913877abea8783d13012340b3
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-12-04 20:41:38 -0600

    Circular-Barn MonteCarlo Solution Feb2016G

---
+++ Gold/February-2016-Gold/Circular-Barn-Revisited.cpp
@@ -0,0 +1,127 @@
+#include <fstream>
+#include <vector>
+#include <deque>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <set>
+#include <ctime>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+    private:
+        struct door {
+            deque<int> cows;
+            long long numCows = 0;
+            long long walkVal = 0;
+            int pos = 1;
+            door(int p) {
+                pos = p;
+            }
+        };
+        int n, k;
+        vector<int> rooms;
+        vector<door*> doors;
+    public:
+        void prepData() {
+
+            ifstream fin ("cbarn2.in");
+            fin >> n >> k;
+            int a;
+            for (int i = 0; i < n; i++) {
+                fin >> a;
+                rooms.push_back(a);
+            }
+
+            set<int> rands;
+            vector<int> js;
+            for (int i = 0; i < k; i++) {
+                int j = rand()%n;
+                while (rands.find(j) != rands.end()) j = rand()%n;
+                rands.insert(j);
+                js.push_back(j);
+            }
+            sort(js.begin(), js.end());
+            for (int i = 0; i < js.size(); i++) {
+                door* b = new door(js[i]);
+                doors.push_back(b);
+            }
+            for (int i = 0; i < doors.size(); i++) {
+                door* currDoor = doors[i];
+                int p = (currDoor->pos+1)%n;
+                int multiplier = 1;
+                while(p != doors[(i+1)%doors.size()]->pos) {
+                    currDoor->cows.push_back(rooms[p]);
+                    currDoor->numCows += rooms[p];
+                    currDoor->walkVal += rooms[p]*multiplier;
+                    p = (p+1)%n;
+                    multiplier += 1;
+                }
+            }
+        }
+        
+        void search() {
+            while (1) {
+                bool unchanged = true;
+
+                for (int i = 0; i < k; i++) {
+                    door* currDoor = doors[i];
+                    door* rightDoor = doors[(i+1)%k];
+                    door* leftDoor = doors[(i-1 == -1 ? k-1 : i-1)];
+                    if ((currDoor->pos+1)%n != rightDoor->pos && (rooms[currDoor->pos]*(leftDoor->cows.size()+1) < currDoor->numCows)) { //adjust right if it makes smaller
+                        int newLeft = rooms[currDoor->pos];
+                        leftDoor->numCows += newLeft;
+                        leftDoor->cows.push_back(newLeft);
+                        leftDoor->walkVal += (leftDoor->cows.size()*newLeft);
+
+                        currDoor->pos = (currDoor->pos+1)%n;    
+                        currDoor->cows.pop_front();
+                        currDoor->walkVal -= (currDoor->numCows);
+                        currDoor->numCows -= rooms[currDoor->pos];
+                        
+                        unchanged = false;
+                    }
+                    if ((leftDoor->pos+1)%n != currDoor->pos && (currDoor->numCows + rooms[currDoor->pos] < leftDoor->cows.back()*leftDoor->cows.size())) { //check adjust curr door left
+                        currDoor->cows.push_front(rooms[currDoor->pos]);
+                        currDoor->numCows += rooms[currDoor->pos];
+                        currDoor->walkVal += (currDoor->numCows);
+                        currDoor->pos = (currDoor->pos-1 == -1 ? n-1 : currDoor->pos-1);
+
+                        int lastSpot = leftDoor->cows.back();
+                        leftDoor->walkVal -= (lastSpot*leftDoor->cows.size());
+                        leftDoor->numCows -= lastSpot;
+                        leftDoor->cows.pop_back();
+
+                        unchanged = false;
+                    }
+                }
+
+                if (unchanged) return;
+            }
+        }
+
+        int main() {
+            prepData();
+            search();
+
+            long long endWalkVal = 0;
+            for (auto i: doors) {
+                endWalkVal += i->walkVal;
+            }
+            return endWalkVal;
+            
+        }
+};
+
+int main() {
+    ofstream fout ("cbarn2.out");
+    srand(time(NULL));
+    long long  min = 0x3FFFFFFFFFFFFFFFLL;
+    for (int i = 0; i < 1000; i++) {
+        Solution s;
+        long long a = s.main();
+        if (a > 0 && a < min) min = a;
+    }
+    fout << min << endl;
+}
---
+++ Gold/February-2016-Gold/Circular-Barn.cpp
@@ -0,0 +1,71 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+    private:
+        int N;
+    public:
+        void Barn() {
+            ifstream fin ("cbarn.in");
+            ofstream fout ("cbarn.out");
+            fin >> N;
+            int q;
+            int front = 0;
+            vector<int> doors;
+            for (int i = 0; i < N; i++) {
+                fin >> q;
+                doors.push_back(q);
+            }
+            for (int i = 0; i <= N; i++) {
+                int j = i-1;
+                if (j < 0) j = N-1;
+                int spots = 0;
+                int cows = 0;
+                while (j != i) {
+                    spots += 1;
+                    cows += doors[j];
+                    j -= 1;
+                    if (j < 0) j = N-1;
+                    if (cows > spots) break;
+                    if (j == i) {
+                        front = i;
+                        break;
+                    }
+                }
+            }
+            vector<int> ordoors;
+            int i = front;
+            int j = i+1;
+            if (j == N) j = 0;
+            ordoors.push_back(doors[i]);  
+            while (j != i) {
+                ordoors.push_back(doors[j]);
+                j++;
+                if (j == N) j = 0;
+            }    
+            long long energy = 0;
+            for (int i = ordoors.size()-2; i >= 0; i--) {
+                while(ordoors[i] > 0 && ordoors[i+1] == 0) {
+                    int e = 0;
+                    for (int j = i; j < ordoors.size()-1; j++) {
+                        if (ordoors[j+1] != 0) break;
+                        ordoors[j] -= 1;
+                        ordoors[j+1] += 1;
+                        e += 1;
+                    }
+                    energy += e*e;
+                }
+            }
+            fout << energy;
+        }
+};
+
+int main() {
+    Solution s;
+    s.Barn();
+
+}
