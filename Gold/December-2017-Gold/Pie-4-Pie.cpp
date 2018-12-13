413731bfb3fd78f457ceb21096c43d97ae481e96
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-12-13 17:43:24 -0600

    Pie-4-Pie Dec2017G

--- Gold/December-2017-Gold/Pie-4-Pie.cpp
+++ Gold/December-2017-Gold/Pie-4-Pie.cpp
@@ -5,13 +5,14 @@
 #include <unordered_set>
 #include <iostream>
 #include <queue>
+#include <functional>
 using namespace std;
 
 class Solution {
     private:
         int N, D;
-        set<pair<int, int>> b;
-        set<pair<int, int>> e;
+        set<pair<int, int>, greater<pair<int, int>>> b;
+        set<pair<int, int>, greater<pair<int, int>>> e;
         vector<pair<int, int>> bStart;
         queue<vector<int>> q;
 
@@ -27,7 +28,6 @@ class Solution {
             }
         };
         unordered_map<pair<int, int>, int, PairHash> shortestB;
-        unordered_set<pair<int, int>, PairHash> eMemo;
     public:
         void prepData() {
             ifstream fin ("piepie.in");
@@ -40,36 +40,29 @@ class Solution {
             }
         }
 
-        //take one pie, then recurse on all the next possible pies to give, when a pie is given back how to 
-        // ensure that we dont regive one of the same pies we have already given
-
         void main() {
             prepData();
             ofstream fout ("piepie.out");   
             int count = 0; 
-            for (auto i: b) if (i.first == 0) q.push({i.first, i.second, 1, 0});
-            for (auto i: e) if (i.first == 0) q.push({i.first, i.second, 0, 1});
+            for (auto i: b) if (i.first == 0) { q.push({i.first, i.second, 1}), b.erase(i); }
+            for (auto i: e) if (i.first == 0) { q.push({i.first, i.second, 0}), e.erase(i); }
             for (auto i: b) shortestB[i] = -1;
             int moves = 0;
             while(!q.empty()) {
                 int size = q.size(); moves++;
-                for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
                     vector<int> curr = q.front(); q.pop();
-                    cout << curr[0] << " " << curr[1] << " " << curr[2] << " " << curr[3] << endl;
                     if (curr[2] == 1) {
-                        if (shortestB[make_pair(curr[0], curr[1])] != -1) continue;
-                        shortestB[make_pair(curr[0], curr[1])] = moves+curr[3];
-                        auto end = e.begin(); end--;
-                        for (auto i = (--e.upper_bound(make_pair(curr[1], 2000000000))); i != end && curr[1] - i->first <= D; i--) {
-                            q.push({i->first, i->second, 0, curr[3]});
+                        shortestB[make_pair(curr[0], curr[1])] = moves;
+                        auto i = (e.lower_bound(make_pair(curr[1], 2000000000)));
+                        while (i != e.end() && curr[1] - i->first <= D) {
+                            q.push({i->first, i->second, 0}); e.erase(i++);
                         }
                     }
                     else {
-                        if (eMemo.find(make_pair(curr[0], curr[1])) != eMemo.end()) continue;
-                        eMemo.insert(make_pair(curr[0], curr[1]));
-                        auto end = b.begin(); end--;
-                        for (auto i = (--b.upper_bound(make_pair(curr[1], 2000000000))); i != end && curr[1] - i->first <= D; i--) {
-                            q.push({i->first, i->second, 1, curr[3]});
+                        auto i = (b.lower_bound(make_pair(curr[1], 2000000000)));
+                        while (i != b.end() && curr[1] - i->first <= D) {
+                            q.push({i->first, i->second, 1}); b.erase(i++);
                         }
                     }
                     
